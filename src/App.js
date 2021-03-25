a28d238b3c34590e231aa3a7b7748bf1d0969a4b
Vivek Chowdary <satyachiratla@gmail.com>
2021-03-25 18:57:21 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import nattu from './img/nattu.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Vivek Chowdary</h1>
+     <h1>My name is Trinadh Nattu</h1>
+     <img width="50%" src={nattu} alt="nattu-img" />
+     <h2> Please Subscribe my youtube channel</h2>
+     <a href="https://youtube.com/channel/UCV_zMzXKyutuMa4rp0U93xg">Pls Subscribe</a>
     </div>
   );
 }
